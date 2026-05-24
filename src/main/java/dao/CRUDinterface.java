ead8ed6d0c82f011af816d1da1c98be70c24fa86
Sara Ruiz <svruiz@uamv.edu.ni>
2026-05-24 13:37:32 -0600

    Modificando CRUD interface

--- src/main/java/dao/CRUDinterface.java
+++ src/main/java/dao/CRUDinterface.java
@@ -2,9 +2,15 @@ package dao;
 
 import java.util.List;
 
-public interface CRUDinterface <T>{
-    public List<T> listar();
-    public void guardar(T object);
+public interface CRUDinterface<T> {
 
+    List<T> listar();
 
+    T buscarPorId(Long id);
+
+    void guardar(T object);
+
+    void actualizar(T object);
+
+    void eliminar(Long id);
 }
--- src/main/java/modelos/Carrera.java
+++ src/main/java/modelos/Carrera.java
@@ -1,9 +1,6 @@
 package modelos;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Positive;
 import lombok.*;
@@ -16,6 +13,7 @@ import lombok.*;
 @ToString
 public class Carrera {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_carrera")
     private Long id;
     @Column(name="nombre_carrera",length = 120, nullable = false)
