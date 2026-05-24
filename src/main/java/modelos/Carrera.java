cdf7bfeebee517f045b1afd329fe1d6f5970a869
Sara Ruiz <svruiz@uamv.edu.ni>
2026-05-24 13:42:05 -0600

    feat: agregar atributo a carrera

--- src/main/java/modelos/Carrera.java
+++ src/main/java/modelos/Carrera.java
@@ -2,23 +2,26 @@ package modelos;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Entity
-@Table(name="carreras")
-@Getter @Setter
+@Table(name = "carreras")
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 public class Carrera {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_carrera")
     private Long id;
-    @Column(name="nombre_carrera",length = 120, nullable = false)
+
+    @Column(name = "nombre_carrera", length = 120, nullable = false)
     private String nombre;
-    @DecimalMin(value = "0.01",message = "Debe ser mayor a 0")
-    @Column(name="precio_carrera")
+
+    @DecimalMin(value = "0.01", message = "Debe ser mayor a 0")
+    @Column(name = "precio_carrera", nullable = false)
     private double precio;
 }
